e3fd8f56292d25edc97588471a9a24b5a57d11d8
prehawk1999 <prehawk@gmail.com>
2014-04-08 22:27:11 +0800

    complete the functions

--- main/Runner.py
+++ main/Runner.py
@@ -2,10 +2,12 @@
 '''
 @author: prehawk
 '''
-import re, json
+import re, json, os, time
 import login
 import urllib2 
 from config import *
+from os.path import join
+from pyquery import PyQuery
 
 class FigureReader(login.Login):
     
@@ -13,39 +15,124 @@ class FigureReader(login.Login):
         super(FigureReader, self).__init__(TEST_USER, TEST_PWD)
         self.figureUrl = url
         
+        self.hosthome   = 'http://weibo.com/p/'
+        self.hostauto   = self.hosthome + 'aj/mblog/mbloglist?'
+        self.pagenum    = 1
+        self.filecount  = 0
         
-        m = re.search('/weibo.com/p/(\d+)/(\w+)\?from=page_(\d+)', url)
+        m = re.search(self.hosthome + '(\d+)/(\w+)\?from=page_(\d+)', url)
         if m:
             self.figureid   = m.group(1)
-            self.tag        = m.group(2)
             self.domain     = m.group(3)
+            self.hostweibo  = self.hosthome + self.figureid + '/weibo?' 
+            
         else:
             print 'raw url parse error'
+            return
               
-        self.weiboUrl = 'http://weibo.com/p/' + self.figureid + '/weibo?from=page_' + self.domain + '&mod=TAB' 
-        doc = urllib2.urlopen(self.weiboUrl).read()
+        self.savedir = '../' + self.figureid
+        if not os.path.exists(self.savedir):
+            os.mkdir(self.savedir)
+            
         
-        jdic = []
+        #self.start()
+        
+    
+    def start(self):
+        
+        url = self.hostweibo + 'from=page_' + self.domain + '&mod=TAB'
+        self.fetchData(url) 
+ 
+ 
+    def fetchFollowlist(self):
+        url = self.hosthome + self.figureid + '/follow?from=page_' + self.domain
+        doc = urllib2.urlopen(url).read() 
+        time.sleep(1)
+        
+        print doc
+        #pl.content.followTab.index
+        jdiclst = []
         m = re.findall('<script>FM\.view\((.*)\);?</script>', doc)
         if m:
             for i in m:
-                jdic.append( json.loads(i) )
-                
-            pass
-            
-            
+                jdiclst.append( json.loads(i) ) 
         else:
             print 'raw doc parse error'
         
+        for i, jdic in enumerate(jdiclst):
+            if 'ns' in jdic:
+                if jdic['ns'] == 'pl.content.followTab.index':
+                    followlist = jdic['html'] 
+                    print followlist 
+                        
         pass
+    
+    
+    def saveWeibo(self):
+        
+        for i in range(2):
+            urlauto = self.hostauto + 'domain=' + self.domain + '&pre_page=' + str(self.pagenum) \
+                + '&page=' + str(self.pagenum) + '&max_id=0&end_id=' + self.endid + '&count=15&pagebar=' + str(i) \
+                + '&max_msign=&filtered_min_id=&pl_name=Pl_Official_LeftProfileFeed__20&id=' + self.figureid \
+                + '&script_uri=/p/' + self.figureid + '/weibo&feed_type=0&from=page_' + self.domain + '&mod=TAB'
+            jsondata = urllib2.urlopen(urlauto).read()
+            time.sleep(1)
             
+            docstr = json.loads(jsondata)['data']
+            with open(join(self.savedir, str(self.filecount) ), 'w') as f:
+                f.write(docstr.encode('utf-8'))
+                self.filecount += 1
+            
+            if i == 1: # encounter the end of the page
+                m = re.search(u'<span>下一页</span></a>', docstr)
+                if m: 
+                    self.pagenum += 1
+                    url = self.hostweibo + \
+                        'pids=Pl_Official_LeftProfileFeed__20&is_search=0&visible=0&is_tag=0&profile_ftype=1&page=' \
+                        + str(self.pagenum)
+                    self.fetchData(url)
+                else:
+                    print 'the end! ' + str(self.pagenum - 1) + 'pages'
+        
         
 
-    def scrapIt(self):
+    def fetchData(self, url):
+         
+        doc = urllib2.urlopen(url).read()
+        time.sleep(1)
         
-        doc = urllib2.urlopen(TEST_URL).read()
-        return doc
+        
+        jdiclst = []
+        m = re.findall('<script>FM\.view\((.*)\);?</script>', doc)
+        if m:
+            for i in m:
+                jdiclst.append( json.loads(i) ) 
+        else:
+            print 'raw doc parse error'
+        
+        
+        for i, jdic in enumerate(jdiclst):
+            if 'ns' in jdic:
+                if jdic['ns'] == 'pl.header.head.index':
+                    head = jdic['html'] 
+                    with open(join(self.savedir, 'info'), 'w') as f:
+                        f.write(head.encode('utf-8'))
+                if jdic['ns'] == 'pl.content.homeFeed.index':
+                    weibo = jdic['html']
+                    n = re.search('mid=\"(\d+)\"', weibo)
+                    if n:
+                        self.endid = n.group(1)
+                    else:
+                        print 'parse error' + str(self.pagenum)
+                    with open(join(self.savedir, str(self.filecount) ), 'w') as f:
+                        f.write(weibo.encode('utf-8'))
+                        self.filecount += 1
+  
+        
+        self.saveWeibo()
+        pass 
     
+
 
 class WeiboItem(object):
     pass
@@ -62,7 +149,6 @@ def transformer():
             b.write(utf8Text)
 
 if __name__ == '__main__':
-    fr = FigureReader(TEST_URL)
-    #print fr.scrapIt()
-    
+    fr = FigureReader('http://weibo.com/p/1035051191258123/home?from=page_103505&mod=TAB#place') 
+    fr.fetchFollowlist()
     
--- main/config.py
+++ main/config.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 #coding=utf8
 
-TEST_URL = 'http://weibo.com/p/1035051003716184/home?from=page_103505&mod=TAB'
+TEST_URL  = 'http://weibo.com/p/1035051003716184/home?from=page_103505&mod=TAB'
+TEST_URL2 = 'http://weibo.com/p/1035051649005320/home?from=page_103505&mod=TAB'
 
 
 
