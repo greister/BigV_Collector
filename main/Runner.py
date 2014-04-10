e9d4e61d9c648b0670479e40b1a6bfc608a68cba
prehawk1999 <prehawk@gmail.com>
2014-04-10 14:00:21 +0800

    solve the recursion problem

--- main/Runner.py
+++ main/Runner.py
@@ -5,53 +5,88 @@
 import re, json, os, time
 import login
 import urllib2 
-from config import *
-from os.path import join
-from pyquery import PyQuery
+import config 
 
-class FigureReader(login.Login):
+
+class Controller(login.Login):
     
-    def __init__(self, url):
-        super(FigureReader, self).__init__(TEST_USER, TEST_PWD)
+    def __init__(self):
+        super(Controller, self).__init__(config.TEST_USER, config.TEST_PWD)
         
+    def start(self):
+ 
+        #FollowListWriter()
+        wr = WeiboReader(2862441992)
+        ret = wr.getWeiboLst()
+        
+        
+        savedir = '../BigVs/'
+        if not os.path.exists(savedir):
+            os.makedirs(savedir)
+            
+        with open( savedir + str(ret[0]), 'w' ) as f:
+            f.write(ret[1].encode('utf-8') + '\n\n')
+            f.write(ret[2].encode('utf-8') )
+         
+            
+    
+
+class FollowListWriter(object):
+    
+    def __init__(self):
+        
+        followSet = set()
+        with open('../followlist', 'r') as f: 
+            for line in f.readlines():
+                followSet.add( line ) 
+ 
+        layerSet = followSet
+        for uid in layerSet:
+            
+            newSet = set( FollowReader(uid).getFollowLst() )
+            writeSet = newSet - followSet
+            followSet.union(newSet)  
+        
+            with open('../followlist', 'a') as f:
+                for w in writeSet:
+                    f.write(w + '\n')
+
+class WeiboFigure(object):
+    
+    def __init__(self, uid):         
        
         self.hosthome   = 'http://weibo.com/'
         self.hostauto   = self.hosthome + 'p/aj/mblog/mbloglist?'
         self.pagenum    = 1
         self.filecount  = 0
-        self.uidlst     = set()
+        self.uid        = uid
         
-        doc = urllib2.urlopen(url).read().decode('string_escape')
-        time.sleep(0.5)
+        url = self.hosthome + 'u/' + str(uid)
+        doc = urllib2.urlopen(url).read().decode('string_escape') 
+        
         m = re.search(r'href=\"\\/p\\/(\d+)\\/(\w+)\?from=page_(\d+)', doc)
         if m:
-            self.figureid   = m.group(1)
+            self.fulluid    = m.group(1)
             self.domain     = m.group(3)
-            self.hostweibo  = self.hosthome + 'p/' + self.figureid + '/weibo?'  
+            self.hostweibo  = self.hosthome + 'p/' + self.fulluid + '/weibo?from=page_'  + self.domain + '&mod=TAB'
+            self.hostfollow = self.hosthome + 'p/' + self.fulluid + '/follow?from=page_' + self.domain + '&page=1'
         else:
             print 'Init: Alias reference page parse error'
             return
+
         
-  
-        self.savedir = '../' + self.figureid
-        if not os.path.exists(self.savedir):
-            os.mkdir(self.savedir)
-            
-         
-        
+
+class FollowReader(WeiboFigure):
     
-    def start(self):
+    def __init__(self, uid):
+        super(FollowReader, self).__init__(uid)
+        self.followLst = []
         
-        url = self.hostweibo + 'from=page_' + self.domain + '&mod=TAB'
-        self.fetchData(url) 
- 
-    def startfollow(self):
-        url = self.hosthome + 'p/' + self.figureid + '/follow?from=page_' + self.domain
-        self.fetchFollowlist(url)
-        pass
- 
-    def fetchFollowlist(self, url):
+    def getFollowLst(self):
+        self._fetchlist( self.hostfollow ) 
+        return self.followLst
         
+    def _fetchlist(self, url):
         doc = urllib2.urlopen(url).read().decode('string_escape')
         time.sleep(0.5) 
           
@@ -60,69 +95,94 @@ class FigureReader(login.Login):
             for i in m: 
                 if re.search('class=\"W_ico16 approve\"', i[1]):
                     userid = re.search('usercard=\"id=(\d+)\"', i[0]).group(1)
-                    self.uidlst.append(userid)
+                    self.followLst.append(userid)
                 else:
-                    continue
-        else:
-            print 'fetchFollowlist: the end of this follow page'
-        
+                    continue 
+
         self.pagenum += 1
         if re.search(r'<span>下一页<\\/span><\\/a>', doc):
-            url = self.hosthome + 'p/' + self.figureid + '/follow?from=page_' + self.domain + '&page=' \
+            url = self.hosthome + 'p/' + self.fulluid + '/follow?from=page_' + self.domain + '&page=' \
                 + str(self.pagenum)
-            self.fetchFollowlist(url)
-        
-    
-    
-    def saveWeibo(self):
-        
-        for i in range(2):
-            urlauto = self.hostauto + 'domain=' + self.domain + '&pre_page=' + str(self.pagenum) \
-                + '&page=' + str(self.pagenum) + '&max_id=0&end_id=' + self.endid + '&count=15&pagebar=' + str(i) \
-                + '&max_msign=&filtered_min_id=&pl_name=Pl_Official_LeftProfileFeed__20&id=' + self.figureid \
-                + '&script_uri=/p/' + self.figureid + '/weibo&feed_type=0&from=page_' + self.domain + '&mod=TAB'
-            jsondata = urllib2.urlopen(urlauto).read()
-            time.sleep(0.5)
-            
-            docstr = json.loads(jsondata)['data']
-            with open(join(self.savedir, str(self.filecount) ), 'w') as f:
-                f.write(docstr.encode('utf-8'))
-                self.filecount += 1
-            
-            if i == 1: # encounter the end of the page
-                m = re.search(u'<span>下一页</span></a>', docstr)
-                if m: 
-                    self.pagenum += 1
-                    url = self.hostweibo + \
-                        'pids=Pl_Official_LeftProfileFeed__20&is_search=0&visible=0&is_tag=0&profile_ftype=1&page=' \
-                        + str(self.pagenum)
-                    self.fetchData(url)
-                else:
-                    print 'the end! ' + str(self.pagenum - 1) + 'pages'
-        
+            self._fetchlist(url)
         
 
-    def fetchData(self, url):
-         
-        doc = urllib2.urlopen(url).read()
-        time.sleep(1)
+class WeiboReader(WeiboFigure):
+    
+    def __init__(self, uid):         
+        super(WeiboReader, self).__init__(uid) 
+
+#         self.savedir = '../BigVs' + self.fulluid
+#         if not os.path.exists(self.savedir):
+#             os.mkdir(self.savedir) 
+            
+        self.phase      = 0
+        self.nextUrl    = self.hostweibo 
+        self.weiboLst   = [self.uid, '', '']
+        
+    def getWeiboLst(self):
+    
+        while self.phase >= 0:
+            
+            self.data = urllib2.urlopen(self.nextUrl).read()
+            time.sleep(config.TIME_GAP)
+            
+            if self.phase == 0:
+                self.fetchNext1()
+            else:
+                self.fetchNext2(self.phase)
         
         
-        jdiclst = []
-        m = re.findall('<script>FM\.view\((.*)\);?</script>', doc)
-        if m:
-            for i in m:
-                jdiclst.append( json.loads(i) ) 
+        return self.weiboLst
+            
+  
+    def makeAutoLink(self, num):
+        return self.hostauto + 'domain=' + self.domain + '&pre_page=' + str(self.pagenum) \
+                + '&page=' + str(self.pagenum) + '&max_id=0&end_id=' + self.endid + '&count=15&pagebar=' + str(num) \
+                + '&max_msign=&filtered_min_id=&pl_name=Pl_Official_LeftProfileFeed__20&id=' + self.fulluid \
+                + '&script_uri=/p/' + self.fulluid + '/weibo&feed_type=0&from=page_' + self.domain + '&mod=TAB'
+                
+    def fetchNext2(self, phase):
+
+        docstr = json.loads(self.data)['data']
+        self.weiboLst[2] += docstr
+        
+        
+#         with open(join(self.savedir, str(self.filecount) ), 'w') as f:
+#             f.write(docstr.encode('utf-8'))
+#             self.filecount += 1
+        
+        if phase == 2: # encounter the end of the page 
+            if re.search(u'<span>下一页</span></a>', docstr):  # has next page
+                self.pagenum += 1
+                self.phase = 0 
+                self.nextUrl = self.hostweibo + \
+                    'pids=Pl_Official_LeftProfileFeed__20&is_search=0&visible=0&is_tag=0&profile_ftype=1&page=' \
+                    + str(self.pagenum) 
+            else: # whole loop end
+                print 'the end! ' + str(self.pagenum - 1) + 'pages'
+                self.phase = -1
+                return
         else:
-            print 'fetchData: raw doc parse error'
+            self.phase = 2
+            self.nextUrl = self.makeAutoLink(1) 
+
+    def fetchNext1(self):
+                 
+        jdiclst = []
+        scripts = re.findall('<script>FM\.view\((.*)\);?</script>', self.data)
+        if scripts:
+            for i in scripts:
+                jdiclst.append( json.loads(i) )
+        else:
+            print 'fetchNext1: raw doc parse error'
         
-        
-        for i, jdic in enumerate(jdiclst):
+        for jdic in jdiclst:
             if 'ns' in jdic:
                 if jdic['ns'] == 'pl.header.head.index':
                     head = jdic['html'] 
-                    with open(join(self.savedir, 'info'), 'w') as f:
-                        f.write(head.encode('utf-8'))
+                    self.weiboLst[1] = head
+#                     with open(join(self.savedir, 'info'), 'w') as f:
+#                         f.write(head.encode('utf-8'))
                 if jdic['ns'] == 'pl.content.homeFeed.index':
                     weibo = jdic['html']
                     n = re.search('mid=\"(\d+)\"', weibo)
@@ -130,21 +190,18 @@ class FigureReader(login.Login):
                         self.endid = n.group(1)
                     else:
                         print 'parse error' + str(self.pagenum)
-                    with open(join(self.savedir, str(self.filecount) ), 'w') as f:
-                        f.write(weibo.encode('utf-8'))
-                        self.filecount += 1
+                        
+                    self.weiboLst[2] += weibo
+                    
+#                     with open(join(self.savedir, str(self.filecount) ), 'w') as f:
+#                         f.write(weibo.encode('utf-8'))
+#                         self.filecount += 1
   
+        self.phase = 1
+        self.nextUrl = self.makeAutoLink(0)
         
-        self.saveWeibo()
-        pass 
     
-
-
-class WeiboItem(object):
-    pass
-
-
-
+  
 def transformer():
     
     with open('../a.txt', 'r') as a:
@@ -155,6 +212,5 @@ def transformer():
             b.write(utf8Text)
 
 if __name__ == '__main__':
-    fr = FigureReader('http://weibo.com/zhangchi19921215')
-    fr.startfollow()
-    
+    c = Controller()
+    c.start()
--- main/config.py
+++ main/config.py
@@ -3,7 +3,7 @@
 
 TEST_URL  = 'http://weibo.com/p/1035051003716184/home?from=page_103505&mod=TAB'
 TEST_URL2 = 'http://weibo.com/p/1035051649005320/home?from=page_103505&mod=TAB'
-
+TIME_GAP  = 0.2
 
 
 
